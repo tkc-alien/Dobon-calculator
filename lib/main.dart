57759ad30ce4044e38e0ce143b2a2f1e2dbfbc38
kouhei-takashima <kouhei-takashima@dip-net.co.jp>
2023-12-03 17:07:16 +0900

    first

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:dobon_calculator/app.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const App());
+}
